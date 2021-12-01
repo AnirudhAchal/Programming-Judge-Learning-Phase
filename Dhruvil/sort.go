3348a4050c1e45d913435244b769c2921d063ec6
Dhruvil Lakhtaria <lakhtariadhruvil123@gmail.com>
2021-12-01 13:12:00 +0530

    wait groups q1

--- Dhruvil/sort.go
+++ Dhruvil/sort.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
+func printSorted(val int) {
+	time.Sleep(time.Duration(val) * time.Millisecond)
+	fmt.Printf("%d ", val)
+}
+func main() {
+	arr := [5]int{3, 6, 4, 2, 5}
+	var wg sync.WaitGroup
+	for _, v := range arr {
+		wg.Add(1)
+		v := v
+		go func() {
+			defer wg.Done()
+			printSorted(v)
+		}()
+	}
+	wg.Wait()
 }
