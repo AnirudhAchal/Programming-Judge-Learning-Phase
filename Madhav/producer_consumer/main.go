5baab339f12a122fad1c99418a6a27023af27aab
Madhav Kumar <lakshy1523@gmail.com>
2021-12-04 20:56:34 +0530

    minor changes

--- Madhav/producer_consumer/main.go
+++ Madhav/producer_consumer/main.go
@@ -11,11 +11,14 @@ var j int = 5
 func producer(mu *sync.Mutex, c *chan int) {
 	for i := 0; i < 10; i++ {
 		mu.Lock()
+		fmt.Println("Producing started")
 		if len(*c) == 5 {
+			fmt.Println("Producing finished")
 			mu.Unlock()
 		} else {
 			*c <- j
 			j = j + 1
+			fmt.Println("Producing finished")
 			mu.Unlock()
 		}
 	}
@@ -24,10 +27,13 @@ func producer(mu *sync.Mutex, c *chan int) {
 func consumer(mu *sync.Mutex, c *chan int) {
 	for i := 0; i < 10; i++ {
 		mu.Lock()
+		fmt.Println("Consuming started")
 		if len(*c) == 0 {
+			fmt.Println("Consuming finished")
 			mu.Unlock()
 		} else {
 			fmt.Println(<-(*c))
+			fmt.Println("Consuming finished")
 			mu.Unlock()
 		}
 	}
--- Madhav/reader_writer/main.go
+++ Madhav/reader_writer/main.go
@@ -15,15 +15,19 @@ var j int = 5
 
 func writer(v *a) {
 	v.Lock()
+	fmt.Println("writing started")
 	v.i = j
 	j++
+	fmt.Println("writing finished")
 	v.Unlock()
 }
 
 func reader(v *a) {
 	for i := 0; i < 10; i++ {
 		v.RLock()
+		fmt.Println("reading started")
 		fmt.Println(v.i)
+		fmt.Println("reading finished")
 		v.RUnlock()
 	}
 }
